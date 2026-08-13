eedbf3caab9896dcf0225da486d2a2ff7cb0eebf
vikash kumar <kmr.vikash.dev@gmail.com>
2026-08-13 23:16:49 +0530

    Complete datatypes foundation lab

--- src/04-sabzi-mandi.js
+++ src/04-sabzi-mandi.js
@@ -48,21 +48,59 @@
  *   removeLastItem(["tamatar", "pyaaz", "mirchi"])   // => "mirchi"
  */
 export function addToCart(cart, item) {
-  // Your code here
+
+  if (!Array.isArray(cart)) {
+    return -1;
+  }
+
+  if (typeof item !== "string" || item.length === 0) {
+    return  cart.length;
+  }
+
+  return cart.push(item);
 }
 
 export function addUrgentItem(cart, item) {
-  // Your code here
+  
+  if (!Array.isArray(cart)) {
+    return [];
+  }
+
+  if (typeof item !== "string" || item.trim() === "") {
+    return cart;
+  }
+
+  cart.unshift(item);
+  
+  return cart;
+
 }
 
 export function removeLastItem(cart) {
-  // Your code here
+
+  if (!Array.isArray(cart) || cart.length === 0) {
+    return undefined;
+  }
+
+  return cart.pop();
+
 }
 
 export function isInCart(cart, item) {
-  // Your code here
+
+  if (!Array.isArray(cart)) {
+    return false;
+  }
+
+  return cart.includes(item);
+
 }
 
 export function mergeCarts(cart1, cart2) {
-  // Your code here
+
+const c1 = Array.isArray(cart1) ? cart1 : [];
+const c2 = Array.isArray(cart2) ? cart2 : [];
+
+return c1.concat(c2);
+
 }
--- src/05-train-coach.js
+++ src/05-train-coach.js
@@ -48,21 +48,51 @@
  *   areAllConfirmed(passengers)          // => true/false
  */
 export function findPassenger(passengers, name) {
-  // Your code here
+
+  if (!Array.isArray(passengers) || typeof name !== "string") {
+    return undefined;
+  }
+
+  return passengers.find(passenger => passenger.name.toLowerCase() === name.toLowerCase() );
+
 }
 
 export function getPassengerIndex(passengers, name) {
-  // Your code here
+
+  if (!Array.isArray(passengers) || typeof name !== "string") {
+    return -1;
+  }
+
+  return passengers.findIndex(passenger => passenger.name.toLowerCase() === name.toLowerCase() );
+
 }
 
 export function isAnyWaitlisted(passengers) {
-  // Your code here
+
+  if (!Array.isArray(passengers) || passengers.length === 0) {
+    return false;
+  }
+
+  return passengers.some(passenger => passenger.status === "waitlisted" );
+
 }
 
 export function areAllConfirmed(passengers) {
-  // Your code here
+
+  if (!Array.isArray(passengers) || passengers.length === 0) {
+    return false;
+  }
+
+  return passengers.every(passenger => passenger.status === "confirmed");
+
 }
 
 export function getWaitlistedPassengers(passengers) {
-  // Your code here
+
+  if (!Array.isArray(passengers)) {
+    return [];
+  }
+
+  return passengers.filter(passenger => passenger.status === "waitlisted" );
+
 }
